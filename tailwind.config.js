6a0319a18da1943ef736d75b6be89d03aadaa3a8
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-04-18 18:26:18 -0500

    Switch colors to default colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        extend: {
+            fontFamily: {
+                SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+            }
+        }
+    }
+};
