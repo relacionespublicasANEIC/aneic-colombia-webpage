6f92d581402ca73c76967e4f3ce603909b8089bf
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-04-27 21:36:20 -0500

    First version. Colombia's map with selectable departaments.

---
+++ src/components/ChapterCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import type { AvaibleSocialMedia } from "../types/interfaces";
+    export let name: string;
+    export let logo: string;
+    export let status: string;
+    export let social_media: AvaibleSocialMedia;
+</script>
+
+<section class="flex flex-col items-center border rounded-lg p-2 border-black">
+    <div class="flex flex-col place-items-center w-full py-2 h-full items-center">
+        <img class="w-20 rounded-full" src={logo} alt={"Logo de " + name} />
+        <h3 class="text-xl text-center w-full">{name}</h3>
+    </div>
+    <div class="flex space-x-2">
+        {#each Object.entries(social_media) as social}
+            <a href={social[1]} target="_blank">
+                <img class="w-6" src={"./icons/" + social[0] + ".svg"} alt={"Icono de " + social[0]} />
+            </a>
+        {/each}
+    </div>
+</section>
+
+<!-- export interface ChapterANEIC {
+    departament: departmentISO;
+    name: string;
+    logo: string;
+    university: {
+        name: string;
+        webpage: string;
+    },
+    social_media: {
+        facebook?: string;
+        instagram?: string;
+        linkedin?: string;
+        twitter?: string;
+        webpage?: string;
+        mail?: string;
+        youtube?: string;
+    }
+} -->
---
+++ src/components/Chapters.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import dataColombia from "./data/ColombiaDepartaments.json";
+    import chapterAneic from "./data/ChaptersANEIC.json";
+    import ChapterCard from "./ChapterCard.svelte";
+
+    let currentDepartament = "CO";
+    $: currentChapters =
+        currentDepartament === "CO" ? chapterAneic : chapterAneic.filter((e) => e.departament === currentDepartament);
+</script>
+
+<div class="relative flex flex-col-reverse items-start md:flex-row">
+    <div class="flex flex-col px-4 py-4 md:py-0 items-center md:w-6/12">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <svg
+            version="1.2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 680 930"
+            class="cursor-pointer w-full"
+            on:click|self={() => {
+                currentDepartament = "CO";
+            }}
+        >
+            <path fill-rule="evenodd" class="fill-none stroke-2" d={dataColombia[0].path} />
+
+            {#each dataColombia.slice(1) as departament (departament.id)}
+                <path
+                    on:click={() => {
+                        currentDepartament = departament.id;
+                    }}
+                    name={departament.name}
+                    fill-rule="evenodd"
+                    d={departament.path}
+                    class:fill-violet-500={departament.id == currentDepartament}
+                    class:opacity-100={departament.id == currentDepartament}
+                    class="fill-orange-400 cursor-pointer stroke-black stroke-1"
+                />
+            {/each}
+        </svg>
+    </div>
+    <div class="flex flex-col md:w-6/12">
+        <h2 class="w-full text-4xl my-4 text-center">
+            {dataColombia.find((e) => e.id === currentDepartament)?.name}
+        </h2>
+
+        <div class="w-full overflow-x-auto">
+            <!-- <select bind:value={currentDepartament}>
+                {#each dataColombia as departament (departament.id)}
+                <option value={departament.id}>{departament.name}</option>
+                {/each}
+            </select> -->
+
+            <div>
+                {#if currentChapters.length === 0}
+                    <div class="flex flex-col items-center p-6">
+                        <img src="/Hero.svg" class="w-3/4" alt="Logo para invitar a personas a unirse">
+                        <h3 class="text-center w-full">
+                            En este departamento todavia no hay capítulos activos.
+                        </h3>
+                    </div>
+                {:else}
+                    <div class="grid gap-4 grid-cols-2 md:grid-cols-3">
+                        {#each currentChapters as chapter}
+                            <ChapterCard {...chapter}></ChapterCard>
+                        {/each}
+                    </div>
+                {/if}
+            </div>
+        </div>
+    </div>
+</div>
