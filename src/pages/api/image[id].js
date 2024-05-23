323bdc39d98331ed9017ecaefa3af0ceb8a6c31b
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-05-22 19:00:59 -0500

    New api endpoints for event display

---
+++ src/pages/api/image[id].js
@@ -0,0 +1,8 @@
+export async function GET({ params }) {
+    const PARAMETERS = new URLSearchParams({
+        key: import.meta.env.CALENDAR_API_KEY,
+        alt: "media"
+    });
+    let res = await fetch(`https://www.googleapis.com/drive/v3/files/${params.id}?${PARAMETERS}`);
+    return new Response(await res.blob());
+}
