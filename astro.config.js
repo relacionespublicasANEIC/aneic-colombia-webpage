e31cede04da1588a50ca921c05e1816e43a91185
Ushi Hiraga <ushi.hiraga@gmail.com>
2024-05-25 00:21:40 -0500

    Using web analitycs now.

--- astro.config.js
+++ astro.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
-export default defineConfig({ integrations: [tailwind(), svelte()], output: "hybrid", adapter: vercel() });
+export default defineConfig({ 
+    integrations: [tailwind(), svelte()], 
+    output: "hybrid",
+    adapter: vercel({ webAnalytics: { enabled: true } })
+});
