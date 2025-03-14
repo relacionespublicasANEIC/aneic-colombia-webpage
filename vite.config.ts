1427346b627e770c1c1a976794885e1d03c10181
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-03-13 21:52:59 -0500

    Use tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({ plugins: [sveltekit(), tailwindcss()] });
