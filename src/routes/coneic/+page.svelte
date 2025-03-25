15219d0b09f50a5431c20d0fa36f1f6f8dab9339
Ushi Hiraga <ushi.hiraga@gmail.com>
2025-03-25 14:15:44 -0500

    Add redirect to coneic 2025 page and remove old coneic2024 reference

---
+++ src/routes/coneic/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <meta http-equiv="refresh" content="0; url=https://coneic.aneiccolombia.com" />
+</svelte:head>
+
+<div class="my-48">
+    <h1>Te estamos llevando a la página oficial del CONEIC</h1>
+</div>
